4262ed545b20959deb2306711b923cee2198622e
mechatroner <mechatroner@yandex.ru>
2017-11-12 23:38:27 -0500

    gapi reliability

--- words_discoverer_chrome/background.js
+++ words_discoverer_chrome/background.js
@@ -56,7 +56,6 @@ function do_load_idioms(file_text) {
 
 
 function load_idioms() {
-    //FIXME code duplication
     file_path = chrome.extension.getURL("eng_idioms.txt");
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -78,8 +77,6 @@ function dbg_list_files() {
     }).then(function(response) {
         console.log('Files:');
         msg_text = 'Files:'
-        console.log("response:" + response); //FOR_DEBUG
-        console.log("response.result:" + response.result); //FOR_DEBUG
         var files = response.result.files;
         if (files && files.length > 0) {
             for (var i = 0; i < files.length; i++) {
@@ -99,9 +96,9 @@ function dbg_list_files() {
 function load_script(url, callback_func) {
   var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
-        if(request.readyState !== 4)
+        if (request.readyState !== 4)
             return;
-        if(request.status !== 200)
+        if (request.status !== 200)
             return;
         eval(request.responseText);
         callback_func();
@@ -112,38 +109,31 @@ function load_script(url, callback_func) {
 
 
 function authorize_user(interactive_authorization) {
-    console.log("authorize_user started"); //FOR_DEBUG
-    // FIXME so apparently the problem is here. We fail to get the token for some reason (although the sign in menu is shown)
-    //chrome.identity.getAuthToken({interactive: interactive_authorization}, function(token) {
-    // FIXME use interactive_authorization parameter insted of "true"
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-        //FIXME check for token, if it is undefined then report an error
-        console.log("Got a token!"); //FOR_DEBUG
-        console.log("token:" + token); //FOR_DEBUG
-        // FIXME apparently token is a string so you can store it in local storage!
+    chrome.identity.getAuthToken({interactive: interactive_authorization}, function(token) {
         if (token === undefined) {
             chrome.runtime.sendMessage({'sync_status': {'message': 'Failed to get oauth token'}});
         } else {
             gapi.client.setToken({access_token: token});
-            console.log("Token was set!"); //FOR_DEBUG
             chrome.runtime.sendMessage({'sync_status': {'message': 'Oauth token initialized'}});
             dbg_list_files();
+            //FIXME set on sync sucess, this was only initialization success
+            chrome.storage.local.set({"wd_gd_sync_active": true});
         }
     });
 }
 
 
 function init_gapi(interactive_authorization) {
-    console.log("init_gapi started"); //FOR_DEBUG
-    api_urls = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"]
-    // FIXME handle gapi.client.init errors
-    // FIXME put the key in a special file
+    console.log("init_gapi started");
+    api_urls = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+    // FIXME create a client-tied key or put the key in a special file 
     init_params = {apiKey: '', discoveryDocs: api_urls};
     gapi.client.init(init_params).then(function() {
         gapi_inited = true;
-        console.log('gapi inited!');
+        console.log('gapi initialization complete');
         authorize_user(interactive_authorization);
     }, function(reject_reason) { 
+        console.log('gapi initialization failed');
         var error_msg = 'Client init request was rejected. reason: ' + reject_reason;
         console.error(error_msg);
         chrome.runtime.sendMessage({'sync_status': {'message': error_msg}});
@@ -152,7 +142,6 @@ function init_gapi(interactive_authorization) {
 
 
 function load_and_init_gapi(interactive_authorization) {
-    console.log("load_and_init_gapi"); //FOR_DEBUG
     load_script('https://apis.google.com/js/api.js', function() {
         gapi.load('client', function() { 
             gapi_loaded = true;
@@ -161,15 +150,16 @@ function load_and_init_gapi(interactive_authorization) {
     });
 }
 
-function ensure_authorization(interactive_authorization) {
-    console.log("gapi_loaded:" + gapi_loaded + ", gapi_inited:" + gapi_inited); //FOR_DEBUG
-    if (!gapi_loaded) {
-        load_and_init_gapi(interactive_authorization);
-    } else if (!gapi_inited) {
-        init_gapi(interactive_authorization);
-    } else {
-        authorize_user(interactive_authorization);
-    }
+function start_sync_sequence(interactive_authorization) {
+    chrome.storage.local.set({"wd_gd_sync_active": false}, function() {
+        if (!gapi_loaded) {
+            load_and_init_gapi(interactive_authorization);
+        } else if (!gapi_inited) {
+            init_gapi(interactive_authorization);
+        } else {
+            authorize_user(interactive_authorization);
+        }
+    });
 }
 
 function initialize_extension() {
@@ -181,7 +171,15 @@ function initialize_extension() {
             sendResponse({wdm_hostname: domain});
         } else if (request.wdm_verdict) {
             if (request.wdm_verdict == "highlight") {
-                chrome.browserAction.setIcon({path: "result48.png", tabId: sender.tab.id});
+                chrome.storage.local.get(['wd_gd_sync_enabled', 'wd_gd_sync_active'], function(result) {
+                    if (result.wd_gd_sync_enabled && result.wd_gd_sync_active) {
+                        chrome.browserAction.setIcon({path: "result_sync48.png", tabId: sender.tab.id});
+                    } else if (result.wd_gd_sync_enabled && !result.wd_gd_sync_active) {
+                        chrome.browserAction.setIcon({path: "result_nosync48.png", tabId: sender.tab.id});
+                    } else {
+                        chrome.browserAction.setIcon({path: "result48.png", tabId: sender.tab.id});
+                    }
+                });
             } else if (request.wdm_verdict == "keyboard") {
                 chrome.browserAction.setIcon({path: "no_dynamic.png", tabId: sender.tab.id});
             } else {
@@ -191,12 +189,11 @@ function initialize_extension() {
             var fullUrl = request.wdm_new_tab_url;
             chrome.tabs.create({'url': fullUrl}, function(tab) { });
         } else if (request.wdm_request == "gd_sync") {
-            //FIXME you also need to setup auth token in non-interactive mode, and if it is failed change the extension icon
-            ensure_authorization(true);
+            start_sync_sequence(true);
         }
     });
 
-    chrome.storage.local.get(['words_discoverer_eng_dict', 'wd_hl_settings', 'wd_online_dicts', 'wd_hover_settings', 'wd_idioms', 'wd_show_percents', 'wd_is_enabled', 'wd_user_vocabulary', 'wd_black_list', 'wd_white_list'], function(result) {
+    chrome.storage.local.get(['words_discoverer_eng_dict', 'wd_hl_settings', 'wd_online_dicts', 'wd_hover_settings', 'wd_idioms', 'wd_show_percents', 'wd_is_enabled', 'wd_user_vocabulary', 'wd_black_list', 'wd_white_list', 'wd_gd_sync_enabled', 'wd_gd_sync_active'], function(result) {
         load_eng_dictionary();
         load_idioms();
         wd_hl_settings = result.wd_hl_settings;
@@ -218,7 +215,9 @@ function initialize_extension() {
         }
         initContextMenus(wd_online_dicts);
 
-        // FIXME if sync is enabled - initialize the gapi
+        if (result.wd_gd_sync_enabled) {
+            start_sync_sequence(false);
+        }
 
         show_percents = result.wd_show_percents;
         if (typeof show_percents === 'undefined') {
